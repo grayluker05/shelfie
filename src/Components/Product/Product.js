598a28554c732cb76736439b9bc5abadefb8329b
grayluker05 <graysonluker@gmail.com>
2019-10-11 10:42:45 -0600

    second commit

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Header from '../Header/Header';
+
+class Product extends Component {
+    render(){
+        return (
+            <div>
+                Product
+            </div>
+        )
+    }
+}
+
+export default Product;
